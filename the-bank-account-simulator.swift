c4bfac8cf8749f7c41c0470acff85a6c918cc08e
tanya <tlathion@icloud.com>
2024-03-28 18:43:51 +0100

    :gear: Setup project file

---
+++ the-bank-account-simulator.swift
@@ -0,0 +1,38 @@
+/*
+----------------- Ce à quoi vous pouvez vous attendre --------------------------
+Vous travaillerez avec des fonctions, des boucles, des instructions conditionnelles,
+des instructions de commutation, des structures et des classes
+pour créer les fonctionnalités du simulateur de compte bancaire.
+
+Les opérations du simulateur de compte bancaire sont implémentées avec des fonctions
+et regroupées dans la structure du projet.
+La logique du simulateur utilise des boucles pour traiter les données de l'utilisateur
+et créer des comptes bancaires valides.
+Les instructions conditionnelles et les instructions de commutation
+mettent en œuvre des opérations de compte bancaire valides
+et veillent à ce que le simulateur de compte bancaire fonctionne sans problème tout au long du processus.
+
+Vous allez créer un simulateur de compte bancaire pour :
+
+- Créer des comptes bancaires valides
+- Vérifier le solde des comptes bancaires
+- Valider les retraits des comptes bancaires et les dépôts sur les comptes bancaires.
+
+En fin de compte, vous coderez un simulateur de compte bancaire en créant un compte bancaire valide
+et en utilisant ensuite une logique de programmation appropriée pour le gérer.
+Vous configurerez le simulateur de compte bancaire pour vérifier le solde du compte bancaire,
+valider les retraits du compte bancaire et surveiller les dépôts sur le compte bancaire.
+--------------------------------------------------------------------------------
+*/
+
+/* +++++++++++++++++ Partie 1: Créer le compte bancaire +++++++++++++++++++++ */
+
+/* ++++++++++ Partie 2: Définir les opérations du compte bancaire +++++++++++ */
+
+/* ++++++++++++++++ Partie 3: Gestion du compte bancaire ++++++++++++++++++++ */
+
+/*
+--------------------------------- TESTS ----------------------------------------
+
+--------------------------------------------------------------------------------
+*/
