7ec6df6b36a3d825124ef593219fb49fea257248
tanya <tlathion@icloud.com>
2024-06-09 20:22:14 +0200

    :rocket: successful exercise on enum

--- EnumExercice.playground/Contents.swift
+++ EnumExercice.playground/Contents.swift
@@ -6,10 +6,21 @@ enum Ingredient: String{
     case jeu="Livre"
 }
 let ingredient = Ingredient.burger
+
 switch ingredient {
-case .frites: print("Je vais prendre des \(ingredient.rawValue).")
-case .burger: print("avec un \(ingredient.rawValue).")
-case .dessert: print("Mettez-moi la \(ingredient.rawValue) comme dessert.")
-case .boisson: print("Comme boisson, je vais prendre un \(ingredient.rawValue).")
-case .jeu: print("Et donnez-moi plutôt le \(ingredient.rawValue) en cadeau.")
+    case .frites: print("Je vais prendre des \(ingredient.rawValue).")
+    case .burger: print("J'aimerais un HappyMeal \(ingredient.rawValue).")
+    case .dessert: print("Mettez-moi la \(ingredient.rawValue) comme dessert.")
+    case .boisson: print("Comme boisson, je vais prendre un \(ingredient.rawValue).")
+    case .jeu: print("Et donnez-moi plutôt le \(ingredient.rawValue) en cadeau.")
+}
+
+enum MealInformation {
+    case choices(information: String)
+}
+
+let mealInformation = MealInformation.choices(information: "Une boisson, un dessert, un cadeau")
+
+switch mealInformation {
+case .choices (let information): print("Vous devez choisir: \(information).")
 }
