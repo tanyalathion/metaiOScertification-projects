c7b1804257d81c42e399c09fa48a8fb39d4817d7
tanya <tlathion@icloud.com>
2024-06-09 21:16:11 +0200

    :rocket: successful exercise

--- SetExercice.playground/Contents.swift
+++ SetExercice.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+let reservationsInPerson:Set<String> = ["000-345-3441", "000-345-3442"]
+let reservationsOverPhone:Set<String> = ["000-345-3443", "000-345-3444"]
+let reservationsOverInternet:Set<String> = ["000-345-3445", "000-345-3446"]
+let inPersonAndOverPhone = reservationsInPerson.union(reservationsOverPhone)
+print(inPersonAndOverPhone)
+var allPhoneNumbers = inPersonAndOverPhone.union(reservationsOverInternet)
+print(allPhoneNumbers)
 
-var greeting = "Hello, playground"
+var confirmationCodes: Set = ["LL3450","LL3451","LL3452", "LL3453", "LL3454"]
+print("\(allPhoneNumbers.count) \(confirmationCodes.count)")
+confirmationCodes.insert("LL3455")
+print(confirmationCodes)
+
+confirmationCodes.remove("LL3451")
+allPhoneNumbers.remove("000-345-3443")
+
+print("\(allPhoneNumbers.count) \(confirmationCodes.count)")
