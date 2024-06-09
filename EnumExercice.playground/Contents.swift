f55fd6e3c6674d340d462e75c00783c381a6ae7b
tanya <tlathion@icloud.com>
2024-06-09 20:07:20 +0200

    Initializing my switch instruction

--- EnumExercice.playground/Contents.swift
+++ EnumExercice.playground/Contents.swift
@@ -5,3 +5,11 @@ enum Ingredient: String{
     case boisson="Coca Zéro"
     case jeu="Livre"
 }
+let ingredient = Ingredient.burger
+switch ingredient {
+case .frites: print("Je vais prendre des \(ingredient.rawValue).")
+case .burger: print("avec un \(ingredient.rawValue).")
+case .dessert: print("Mettez-moi la \(ingredient.rawValue) comme dessert.")
+case .boisson: print("Comme boisson, je vais prendre un \(ingredient.rawValue).")
+case .jeu: print("Et donnez-moi plutôt le \(ingredient.rawValue) en cadeau.")
+}
