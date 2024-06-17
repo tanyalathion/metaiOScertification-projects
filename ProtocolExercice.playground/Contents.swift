9789997cda2b2a0195e2449bfcb85f09f7d29766
tanya <tlathion@icloud.com>
2024-06-17 21:41:26 +0200

    :minidisc: Exercice mid commit

--- ProtocolExercice.playground/Contents.swift
+++ ProtocolExercice.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+protocol Dish {
+    var name: String {get}
+    var preparationMinutes: Int {get set}
+    func prepare ()
+    func plate (artisticLevel: Int) -> Int
+}
+
+class MainDish: Dish {
+    let name: String
+    var preparationMinutes: Int
+    init(name: String, preparationMinutes: Int) {
+        self.name = name
+        self.preparationMinutes = preparationMinutes
+    }
+    func prepare() {
+        print("Preparing \(name) for \(preparationMinutes) minutes")
+    }
+    func plate(artisticLevel: Int) {
+        print("Plating artistic level: \(artisticLevel)")
+    }
+}
+
+let superSpaghetti = MainDish(
+    name: "Super Spaghetti",
+    preparationMinutes: 35
+)
+
