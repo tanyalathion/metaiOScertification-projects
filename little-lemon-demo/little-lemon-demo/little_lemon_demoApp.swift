e6c5f839bf86852fbfb7978109298d5ec78c649f
tanya <tlathion@icloud.com>
2024-06-19 21:58:15 +0200

    :star: New final project set up

---
+++ little-lemon-demo/little-lemon-demo/little_lemon_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_lemon_demoApp.swift
+//  little-lemon-demo
+//
+//  Created by Tanya Lathion on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct little_lemon_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
