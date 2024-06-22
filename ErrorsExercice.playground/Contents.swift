ba348e457d0b1f3977adf844013c3d3898555493
tanya <tlathion@icloud.com>
2024-06-22 20:22:26 +0200

    :lady_beetle: New exercice about errors

---
+++ ErrorsExercice.playground/Contents.swift
@@ -0,0 +1,3 @@
+//Supposons que vous créiez une application de calculatrice. Pour signaler les erreurs liées à des opérations mathématiques non valides, vous écrirez des fonctions jetables. Vous appellerez également la fonctionnalité et gérerez les erreurs à l'aide de do-catch.
+
+
